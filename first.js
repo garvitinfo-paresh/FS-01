c6323b09894401deb8974fed8f3c24ab71de24c7
PareshRajput <garvitinfo.paresh@gmail.com>
2026-05-13 15:31:01 +0530

    13-05-26

---
+++ first.js
@@ -0,0 +1,132 @@
+// 11/05/26
+// console.log("Hello");
+
+// 12/05/26
+// console.log("1. Hello");
+// console.log("2. Hello");
+// console.log("3. Hello");
+// console.log("4. Hello");
+// console.log("5. Hello");
+
+// console.log(1 + 3)
+// console.log(10 / 3) //
+// console.log(10 / 2) //
+// console.log(10 % 3) //
+// console.log(0.10 + 0.3) //
+// console.log(0.1 + 0.3) //
+// console.log(0.1 + 0.3) //
+
+// console.log(typeof (10 / 3)) //
+// console.log(typeof (10 / 2)) //
+// console.log(typeof ('hello')) //
+// console.log(typeof (true)) //
+// console.log(typeof (true)) //
+
+// Boolean
+
+// true=1 false=0
+
+// console.log(typeof (false * 15))
+// console.log(typeof (false * 15))
+// console.log(typeof (true * 15))
+// console.log((true * 15))
+
+// console.log(true + true)
+// console.log((true + true + true) * false)
+// 'hello' -> string
+// console.log(typeof ('true'))
+// console.log(typeof ('1'))
+// console.log('10' * '20')
+// console.log('100' / '20')
+// console.log('100' - '20')
+// console.log(typeof ('10' + '20'))
+
+// console.log('10' + 10)
+// console.log(+'10' + 10)
+// console.log(10 + '10')
+// console.log(10 + +'10')
+// console.log(10 - '10')
+// console.log(10 * '10') //100
+
+//  NaN = Not a Number
+// console.log(10 * 'ten')//NaN
+// console.log(10 + 'ten')//10ten
+// console.log(10 - 'ten')//NaN
+// console.log(10 / 'ten')//NaN
+
+// console.log(typeof (10 / 'ten')) //NaN
+// console.log(typeof (NaN))
+
+
+// For declaring variable there is 3 keywords
+// var(default) let const
+// int x;
+// x = 10
+// var x = true
+
+// console.log(typeof (x), '-', x)
+
+// String declaration
+// using singlequote  ' '
+// using doublequote  " "
+// using bacltick  ` `
+//          introduced es6 - template literal - multiple line / paragraph
+//
+
+// var strSQ = 'Lorem ipsum, dolor sit amet     consectetur adipisicing elit.                    Tempore est, omnis, possimus incidunt repellendus aperiam odit maxime iure accusamus ducimus ex.'
+
+// var strDQ = "Lorem ipsum, dolor sit amet     consectetur adipisicing elit.                    Tempore est, omnis, possimus incidunt repellendus aperiam odit maxime iure accusamus ducimus ex."
+
+// var strBT = `Lorem
+// ipsum, dolor sit amet     consectetur adipisicing elit.
+//                     Tempore est, omnis, possimus incidunt repellendus aperiam odit
+//              maxime iure accusamus ducimus ex.`
+
+// console.log(strDQ)
+// console.log(strSQ)
+// console.log(strBT)
+
+
+// 13/05/26
+
+// ``
+
+// var str1 = " let's "
+// var str2 = ' let"s '
+
+// console.log(str1, "-", str2)
+
+
+// Data types in JS
+
+// console.log(typeof ("Hello")) //string
+// console.log(typeof 100)      //number
+// console.log(typeof true)    //boolean
+
+// var x;
+// console.log(typeof x) //undefined
+
+// console.log(typeof []) //object
+// console.log(typeof {}) //object
+// console.log(typeof (() => { })) //function
+// console.log(typeof (function () { })) //function
+
+// console.log(typeof null) //object
+
+// console.log(typeof (10 / 'ten')) //NaN - number
+
+// console.log(typeof 999)  //number
+// console.log(typeof 999n) //bigint
+
+// var myInt = 989898989898998989898965686568956598659989898989898989989898989656865689565986599898989898989899898989896568656895659865998989898989898998989898965686568956598659989898989898989989898989656865689565986599898989898989899898989896568656895659865998989898989898998989898965686568956598659989898989898989989898989656865689565986599898989898989899898989896568656895659865998989898989898998989898965686568956598659989898989898989989898989656n;
+
+
+// var myInt = 1;
+
+// console.log(typeof myInt)
+
+
+// console.log(typeof Symbol(myInt)) //symbol
+
+
+
