8157df82f34ef09a5aff3709289979a5057d90da
PareshRajput <garvitinfo.paresh@gmail.com>
2026-06-06 14:38:10 +0530

    06-06-2026

--- callback_promise.js
+++ callback_promise.js
@@ -62,7 +62,6 @@
 //     })
 // })
 
-
 // https://www.freecodecamp.org/news/javascript-async-await-tutorial-learn-callbacks-promises-async-await-by-making-icecream/
 
 // let stocks = {
@@ -116,3 +115,80 @@
 // order(0, production);
 
 // This is called callback hell.
+
+// let myPromise = new Promise((res, rej) => {
+//     setTimeout(() => {
+//         console.log("Hello")
+//         // console.log(`${res("hello")}`)
+//     }, 500)
+// })
+// myPromise
+//     .then(1, 0)
+//     .catch((err) => {
+//         console.log(err)
+//     })
+
+// console.log(myPromise)
+
+
+// let is_shop_open = true;
+
+// let order = (time, work) => {
+//     return new Promise((resolve, reject) => {
+//         if (is_shop_open) {
+//             resolve()
+//         }
+//         else {
+//             reject(console.log("Our shop is closed"))
+//         }
+//     })
+// }
+// console.log(order())
+
+let stocks = {
+    Fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"],
+};
+
+let is_shop_open = true;
+
+let order = (time, work) => {
+    return new Promise((resolve, reject) => {
+        if (is_shop_open) {
+            setTimeout(() => {
+                // console.log(`${work}`)
+                resolve(work())
+            }, time)
+        }
+        else {
+            reject(console.log("Our shop is closed"))
+        }
+    })
+}
+order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
+    .then(() => {
+        return order(500, () => console.log('production has started'))
+    })
+    .then(() => {
+        return order(2000, () => console.log("Fruit has been chopped"))
+    })
+    .then(() => {
+        return order(1000, () => console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`))
+    })
+    .then(() => {
+        return order(1000, () => console.log("start the machine"))
+    })
+    .then(() => {
+        return order(2000, () => console.log(`ice cream placed on ${stocks.holder[1]}`))
+    })
+    .then(() => {
+        return order(3000, () => console.log(`${stocks.toppings[0]} as toppings`))
+    })
+    .then(() => {
+        return order(2000, () => console.log("Serve Ice Cream"))
+    })
+    .catch(() => {
+        console.log("Customer left")
+    })
